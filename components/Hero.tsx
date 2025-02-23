8e8aaa7ede637612591f86c7f0a491ec0b12aaf5
gsmkev <gsmkev@gmail.com>
2025-02-23 12:35:33 -0300

    feat(hero): add animations to Hero component for improved user engagement

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,12 +3,18 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 export default function Hero() {
 	return (
 		<section>
 			<div className="grid grid-cols-1 sm:grid-cols-12">
-				<div className="col-span-7 place-self-center text-center sm:text-left justify-self-start">
+				<motion.div
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.5 }}
+					className="col-span-7 place-self-center text-center sm:text-left justify-self-start"
+				>
 					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
 						<span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
 							Hello, I'm{" "}
@@ -46,8 +52,13 @@ export default function Hero() {
 							</span>
 						</Button>
 					</div>
-				</div>
-				<div className="col-span-5 place-self-center sm:pl-10 mt-4 lg:mt-0">
+				</motion.div>
+				<motion.div
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.5 }}
+					className="col-span-5 place-self-center sm:pl-10 mt-4 lg:mt-0"
+				>
 					<div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
 						<Image
 							src="/images/hero.png"
@@ -57,7 +68,7 @@ export default function Hero() {
 							height={200}
 						/>
 					</div>
-				</div>
+				</motion.div>
 			</div>
 		</section>
 	);
